13de154d51476c314eb3f3ddb7283bf217b4672d
sotowang <sotowang@qq.com>
2018-11-25 21:54:35 +0800

    页面单跳转化率模块spark作业:获取页面流中初始页面的pv

--- src/main/java/com/soto/spark/page/PageOneStepConvertRateSpark.java
+++ src/main/java/com/soto/spark/page/PageOneStepConvertRateSpark.java
@@ -5,15 +5,22 @@ import com.soto.constant.Constants;
 import com.soto.dao.ITaskDAO;
 import com.soto.dao.impl.DAOFactory;
 import com.soto.domain.Task;
+import com.soto.util.DateUtils;
 import com.soto.util.ParamUtils;
 import com.soto.util.SparkUtils;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+import scala.Tuple2;
 
-import java.util.Date;
+import java.util.*;
 
 /**
  * 页面单跳转化率模块spark作业
@@ -52,8 +59,195 @@ public class PageOneStepConvertRateSpark {
         //4.查询指定日期范围内的用户访问行为数据
         JavaRDD<Row> actionRDD = SparkUtils.getActionRDDByDateRange(sqlContext, taskParam);
 
+        JavaPairRDD<String, Row> sessionid2actionRDD = getSessionid2actionRDD(actionRDD);
+        sessionid2actionRDD = sessionid2actionRDD.cache(); // persist(StorageLevel.MEMORY_ONLY)
+
+        // 对<sessionid,访问行为> RDD，做一次groupByKey操作
+        // 因为要拿到每个session对应的访问行为数据，才能够去生成切片
+        JavaPairRDD<String, Iterable<Row>> sessionid2actionsRDD = sessionid2actionRDD.groupByKey();
+
+        // 最核心的一步，每个session的单跳页面切片的生成，以及页面流的匹配，算法
+        JavaPairRDD<String, Integer> pageSplitRDD = generateAndMatchPageSplit(
+                sc, sessionid2actionsRDD, taskParam);
+        Map<String, Object> pageSplitPvMap = pageSplitRDD.countByKey();
+
+
+
+
+
 
 
 
     }
+
+
+    /**
+     * 获取<sessionid,用户访问行为>格式的数据
+     * @param actionRDD 用户访问行为RDD
+     * @return <sessionid,用户访问行为>格式的数据
+     */
+    private static JavaPairRDD<String, Row> getSessionid2actionRDD(
+            JavaRDD<Row> actionRDD) {
+        return actionRDD.mapToPair(new PairFunction<Row, String, Row>() {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Tuple2<String, Row> call(Row row) throws Exception {
+                String sessionid = row.getString(2);
+                return new Tuple2<String, Row>(sessionid, row);
+            }
+
+        });
+    }
+
+
+    /**
+     * 页面切片生成与匹配算法
+     * @param sc
+     * @param sessionid2actionsRDD
+     * @param taskParam
+     * @return
+     */
+    private static JavaPairRDD<String, Integer> generateAndMatchPageSplit(
+            JavaSparkContext sc,
+            JavaPairRDD<String, Iterable<Row>> sessionid2actionsRDD,
+            JSONObject taskParam) {
+        String targetPageFlow = ParamUtils.getParam(taskParam, Constants.PARAM_TARGET_PAGE_FLOW);
+        final Broadcast<String> targetPageFlowBroadcast = sc.broadcast(targetPageFlow);
+
+        return sessionid2actionsRDD.flatMapToPair(
+
+                new PairFlatMapFunction<Tuple2<String,Iterable<Row>>, String, Integer>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Iterable<Tuple2<String, Integer>> call(
+                            Tuple2<String, Iterable<Row>> tuple)
+                            throws Exception {
+                        // 定义返回list
+                        List<Tuple2<String, Integer>> list =
+                                new ArrayList<Tuple2<String, Integer>>();
+                        // 获取到当前session的访问行为的迭代器
+                        Iterator<Row> iterator = tuple._2.iterator();
+                        // 获取使用者指定的页面流
+                        // 使用者指定的页面流，1,2,3,4,5,6,7
+                        // 1->2的转化率是多少？2->3的转化率是多少？
+                        String[] targetPages = targetPageFlowBroadcast.value().split(",");
+
+                        // 这里拿到的session的访问行为，默认情况下是乱序的
+                        // 比如说，正常情况下，希望拿到的数据，是按照时间顺序排序的
+                        // 但是问题是，默认是不排序的
+                        // 所以，第一件事情，对session的访问行为数据按照时间进行排序
+
+                        // 举例，反例
+                        // 比如，3->5->4->10->7
+                        // 3->4->5->7->10
+                        // 排序
+
+                        List<Row> rows = new ArrayList<Row>();
+                        while(iterator.hasNext()) {
+                            rows.add(iterator.next());
+                        }
+
+                        Collections.sort(rows, new Comparator<Row>() {
+
+                            @Override
+                            public int compare(Row o1, Row o2) {
+                                String actionTime1 = o1.getString(4);
+                                String actionTime2 = o2.getString(4);
+
+                                Date date1 = DateUtils.parseTime(actionTime1);
+                                Date date2 = DateUtils.parseTime(actionTime2);
+
+                                return (int)(date1.getTime() - date2.getTime());
+                            }
+
+                        });
+
+                        // 页面切片的生成，以及页面流的匹配
+                        Long lastPageId = null;
+
+                        for(Row row : rows) {
+                            long pageid = row.getLong(3);
+
+                            if(lastPageId == null) {
+                                lastPageId = pageid;
+                                continue;
+                            }
+
+                            // 生成一个页面切片
+                            // 3,5,2,1,8,9
+                            // lastPageId=3
+                            // 5，切片，3_5
+
+                            String pageSplit = lastPageId + "_" + pageid;
+
+                            // 对这个切片判断一下，是否在用户指定的页面流中
+                            for(int i = 1; i < targetPages.length; i++) {
+                                // 比如说，用户指定的页面流是3,2,5,8,1
+                                // 遍历的时候，从索引1开始，就是从第二个页面开始
+                                // 3_2
+                                String targetPageSplit = targetPages[i - 1] + "_" + targetPages[i];
+
+                                if(pageSplit.equals(targetPageSplit)) {
+                                    list.add(new Tuple2<String, Integer>(pageSplit, 1));
+                                    break;
+                                }
+                            }
+
+                            lastPageId = pageid;
+                        }
+
+                        return list;
+                    }
+
+                });
+    }
+
+
+    /**
+     * 获取页面流中初始页面的pv
+     * @param taskParam
+     * @param sessionid2actionsRDD
+     * @return
+     */
+    private static long getStartPagePv(JSONObject taskParam,
+                                       JavaPairRDD<String, Iterable<Row>> sessionid2actionsRDD) {
+        String targetPageFlow = ParamUtils.getParam(taskParam,
+                Constants.PARAM_TARGET_PAGE_FLOW);
+        final long startPageId = Long.valueOf(targetPageFlow.split(",")[0]);
+
+        JavaRDD<Long> startPageRDD = sessionid2actionsRDD.flatMap(
+
+                new FlatMapFunction<Tuple2<String,Iterable<Row>>, Long>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Iterable<Long> call(
+                            Tuple2<String, Iterable<Row>> tuple)
+                            throws Exception {
+                        List<Long> list = new ArrayList<Long>();
+
+                        Iterator<Row> iterator = tuple._2.iterator();
+
+                        while(iterator.hasNext()) {
+                            Row row = iterator.next();
+                            long pageid = row.getLong(3);
+
+                            if(pageid == startPageId) {
+                                list.add(pageid);
+                            }
+                        }
+
+                        return list;
+                    }
+
+                });
+
+        return startPageRDD.count();
+    }
+
 }

5c8dd06c4c4dc9ed9e3d39ba9700025610c43ffb
sotowang <sotowang@qq.com>
2018-11-27 16:29:40 +0800

    区域top3商品:使用Spark SQL从MySQL中查询城市信息

--- src/main/java/com/soto/spark/product/AreaTop3ProducrSpark.java
+++ src/main/java/com/soto/spark/product/AreaTop3ProducrSpark.java
@@ -1,6 +1,7 @@
 package com.soto.spark.product;
 
 import com.alibaba.fastjson.JSONObject;
+import com.soto.conf.ConfigurationManager;
 import com.soto.constant.Constants;
 import com.soto.dao.ITaskDAO;
 import com.soto.dao.impl.DAOFactory;
@@ -8,11 +9,17 @@ import com.soto.domain.Task;
 import com.soto.util.ParamUtils;
 import com.soto.util.SparkUtils;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
+import scala.Tuple2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 各区域top3热门商品统计Spark作业
@@ -58,7 +65,10 @@ public class AreaTop3ProducrSpark {
         String endDate = ParamUtils.getParam(taskParam, Constants.PARAM_END_DATE);
 
 
-        JavaRDD<Row> clickActionRDD = getClickActionRDDByDate(sqlContext, startDate, endDate);
+        JavaPairRDD<Long,Row> clickActionRDD = getcityid2ClickActionRDDByDate(sqlContext, startDate, endDate);
+
+        // 从MySQL中查询城市信息
+        JavaPairRDD<Long, Row> cityInfoRDD = getcityid2CityInfoRDD(sqlContext);
 
 
         sc.close();
@@ -67,12 +77,13 @@ public class AreaTop3ProducrSpark {
 
     /**
      * 查询指定日期范围内的点击行为数据
+     *
      * @param sqlContext
-     * @param startDate 起始日期
-     * @param endDate  截止日期
+     * @param startDate  起始日期
+     * @param endDate    截止日期
      * @return
      */
-    public static JavaRDD<Row> getClickActionRDDByDate(SQLContext sqlContext, String startDate, String endDate) {
+    private static JavaPairRDD<Long, Row> getcityid2ClickActionRDDByDate(SQLContext sqlContext, String startDate, String endDate) {
 
         // 从user_visit_action中，查询用户访问行为数据
         // 第一个限定：click_product_id，限定为不为空的访问行为，那么就代表着点击行为
@@ -89,8 +100,78 @@ public class AreaTop3ProducrSpark {
                         + "AND action_time<='" + endDate + "'";
         DataFrame clickActionDF = sqlContext.sql(sql);
 
+        JavaRDD<Row> clickActionRDD = clickActionDF.javaRDD();
 
-        return clickActionDF.javaRDD();
+        JavaPairRDD<Long, Row> cityid2clickActionRDD = clickActionRDD.mapToPair(
+
+                new PairFunction<Row, Long, Row>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Tuple2<Long, Row> call(Row row) throws Exception {
+                        Long cityid = row.getLong(0);
+                        return new Tuple2<Long, Row>(cityid, row);
+                    }
+
+                });
+
+        return cityid2clickActionRDD;
+    }
+
+    /**
+     * 使用Spark SQL从MySQL中查询城市信息
+     *
+     * @param sqlContext
+     * @return
+     */
+    private static JavaPairRDD<Long, Row> getcityid2CityInfoRDD(SQLContext sqlContext) {
+        // 构建MySQL连接配置信息（直接从配置文件中获取）
+        String url = null;
+        String user = null;
+        String password = null;
+        boolean local = ConfigurationManager.getBoolean(Constants.SPARK_LOCAL);
+
+        if (local) {
+            url = ConfigurationManager.getProperty(Constants.JDBC_URL);
+            user = ConfigurationManager.getProperty(Constants.JDBC_USER);
+            password = ConfigurationManager.getProperty(Constants.JDBC_PASSWORD);
+        } else {
+            url = ConfigurationManager.getProperty(Constants.JDBC_URL_PROD);
+            user = ConfigurationManager.getProperty(Constants.JDBC_USER_PROD);
+            password = ConfigurationManager.getProperty(Constants.JDBC_PASSWORD_PROD);
+        }
+
+        Map<String, String> options = new HashMap<String, String>();
+        options.put("url", url);
+        options.put("dbtable", "city_info");
+        options.put("user", user);
+        options.put("password", password);
+
+
+        // 通过SQLContext去从MySQL中查询数据
+        DataFrame cityInfoDF = sqlContext.read().format("jdbc")
+                .options(options).load();
+
+        // 返回RDD
+        JavaRDD<Row> cityInfoRDD =  cityInfoDF.javaRDD();
+
+
+        JavaPairRDD<Long, Row> cityid2cityInfoRDD = cityInfoRDD.mapToPair(
+
+                new PairFunction<Row, Long, Row>() {
+
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public Tuple2<Long, Row> call(Row row) throws Exception {
+                        long cityid = Long.valueOf(String.valueOf(row.get(0)));
+                        return new Tuple2<Long, Row>(cityid, row);
+                    }
+
+                });
+
+        return cityid2cityInfoRDD;
 
     }
 }
